a7ff639488eaac5a8cd465025d1c0043fe178c8c
Punyaphat Surakiatkamjorn <punyaphat.sura@gmail.com>
2025-01-09 19:15:40 +0700

    Add current and total amount

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -162,6 +162,9 @@ const Home: FC = () => {
             </div>
 
             <div className='space-y-2'>
+              <p className='text-gray-500 text-sm font-normal '>
+                {currentRow + 1} / {data.length}
+              </p>
               <div className='flex justify-between text-sm text-gray-600'>
                 <span>Progress: {progress}%</span>
                 <span>Remaining: {data.length - currentRow}</span>
